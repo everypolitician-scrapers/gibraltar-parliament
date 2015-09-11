1e16316ddf709385034d1063d5818e78dc6b0f27
Andy Lulham <a.lulham@gmail.com>
2015-09-11 23:32:30 +0100

    Update scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,66 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# -*- coding: utf-8 -*-
+import re
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import bs4
+import scraperwiki
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+
+url = "http://www.parliament.gi/history/composition-of-parliament"
+r = scraperwiki.scrape(url)
+soup = bs4.BeautifulSoup(r, "html.parser")
+
+term_re = re.compile(r"(?:Footnote:|([^ ]+ (?:House of Assembly|Gibraltar Parliament)) \((.*?) . (.*?)\))")
+position_re = re.compile(r"(?:GOVERNMENT|OPPOSITION|SPEAKER|CLERK)")
+name_re = re.compile(r"The Hon .*$")
+name_and_role_re = re.compile(ur".*?The Hon (.*?)(?: (?:-|–) (.*))?$")
+name_and_role_first_term_re = re.compile(r"(.*?)(?:, ((?:Minister|Chief Minister|Attorney General|Financial & Development Secretary|Leader).*))?$")
+
+terms = soup.find_all(text=term_re)
+id_ = 0
+terms_list = []
+for term in terms:
+    term_name, start_date, end_date = term_re.match(term).groups()
+    if not term_name:
+        # this is just here to deal with the weird bit in
+        # the Eighth House of Assembly
+        continue
+    id_ += 1
+    terms_list.append({
+        "id": id_,
+        "name": term_name,
+        "start_date": start_date,
+        "end_date": end_date,
+    })
+
+scraperwiki.sqlite.save(["id"], terms_list, "terms")
+terms_dict = {x["name"]: x for x in terms_list}
+
+members = soup.find_all(text=name_re)
+
+data_list = []
+for member in members:
+    position = member.find_previous(text=position_re)
+    if position in ["SPEAKER", "CLERK"]:
+        continue
+    parliament = member.find_previous(text=term_re)
+    if parliament.startswith("Footnote"):
+        # this is just here to deal with the weird bit in
+        # the Eighth House of Assembly
+        continue
+    term_name = term_re.match(parliament).group(1)
+    term_id = terms_dict[term_name]["id"]
+    name, role = name_and_role_re.match(member.strip()).groups()
+    if term_name == "First House of Assembly":
+        name, role = name_and_role_first_term_re.match(name).groups()
+    data_list.append({
+        "name": name.strip(),
+        "area": None,  # we don't have this data
+        "group": None,  # we don't have this data
+        "term": term_id,
+        "start_date": "",
+        "end_date": "",
+        "role": role.strip() if role else None,
+        "side": position.title(),
+    })
+
+scraperwiki.sqlite.save(["name", "term"], data_list, "data")
